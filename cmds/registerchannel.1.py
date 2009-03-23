b1d9d2d49f276fff2cd3c0a7e5069c2ba9945250
tiziano <tiziano@tiziano-desktop-amd64.(none)>
2009-03-23 23:15:15 +0100

    fix

--- cmds/registerchannel.1.py
+++ cmds/registerchannel.1.py
@@ -5,4 +5,4 @@ if len(args) == 2:
     c.send("SERVERMSG %s\n" % "Not enough  access level to register this channel")  #cl.username != self.main.channels[args[1]].founder --- should i enable registering channel to anyone? 
   else:
     self.main.channels[args[1]].confirm(self.main.database)
-    self.main.broadcastchannel(args[1],"CHANNELMESSAGE %s %s\n" % (args[1],"This channel has just been saved to database")
+    self.main.broadcastchannel(args[1],"CHANNELMESSAGE %s %s\n" % (args[1],"This channel has just been saved to database"))
