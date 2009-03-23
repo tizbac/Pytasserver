8c250f5c109c450eba300516290616f499df3c97
tiziano <tiziano@tiziano-desktop-amd64.(none)>
2009-03-23 22:01:35 +0100

    Added channelfounder command: 	CHANNELFOUNDER [channel] [newfounder]

---
+++ cmds/channelfounder.py
@@ -0,0 +1,4 @@
+if len(args) == 3 and self.main.sql and args[1] in self.main.channels and self.main.channels[args[1]].confirmed and ( cl.mod == 1 or cl.username == self.main.channels[args[1]].founder ):
+  self.main.channels[args[1]].founder = args[2]
+  self.main.channels[args[1]].sync(self.main.database)
+  self.main.broadcastchannel(args[1],"CHANNELMESSAGE %s <%s> is the new founder of this channel\n" % (args[1],args[2]))
