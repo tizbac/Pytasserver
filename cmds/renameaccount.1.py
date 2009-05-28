3c1fa81ab32a25097d24e0495aff02564fe2d594
tiziano <tiziano@tiziano-desktop-amd64.(none)>
2009-05-28 19:34:58 +0200

    Missing check on renameaccount

--- cmds/renameaccount.1.py
+++ cmds/renameaccount.1.py
@@ -2,10 +2,13 @@
 if self.main.sql and cl.sql:
   if len(args) == 2:
     if not self.main.getaccountid(args[1].lower()):
-      cl.username = args[1]
-      c.send("SERVERMSG Account name changed succesfully\n")
-      cl.sync(self.main.database)
-      self.remove(co,"Renaming account")
+      if not args[1].lower() in self.main.clientsusernames:
+	cl.username = args[1]
+	c.send("SERVERMSG Account name changed succesfully\n")
+	cl.sync(self.main.database)
+	self.remove(co,"Renaming account")
+      else:
+	c.send("SERVERMSG Error, an user with that name is already logged in\n")
     else:
       c.send("SERVERMSG Error, an user with that name already exists\n")
 else:
