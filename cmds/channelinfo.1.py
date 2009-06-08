03b223d24c23631d2ed6e314d30672c8c534f5dc
tiziano <tiziano@tiziano-desktop-amd64.(none)>
2009-06-08 19:12:18 +0200

    fixed channelinfo command

--- cmds/channelinfo.1.py
+++ cmds/channelinfo.1.py
@@ -5,5 +5,6 @@
 if len(args) >= 2 and args[1] in self.main.channels:
   cha = self.main.channels[args[1]]
   c.send("SERVERMSG Channel #%s: Founder is <%s>\n" % (args[1],self.main.getaccountbyid(cha.founder)))
-  ops = ', '.join(cha.operators)
+  for o in cha.operators:
+    ops += self.main.getaccountbyid(int(o))+', '
   c.send("SERVERMSG Channel #%s: Operators are : %s\n" % (args[1],ops))
