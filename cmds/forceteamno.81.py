5e6bc008d418722fc4eed0800f2964e5adb9d365
tiziano <tiziano@tiziano-desktop-amd64.(none)>
2009-08-12 18:38:11 +0200

    fix forceteamno

--- cmds/forceteamno.81.py
+++ cmds/forceteamno.81.py
@@ -7,7 +7,7 @@ if len(args) == 3:
   if cl.battle in self.main.battles and self.main.battles[cl.battle].founder == cl.username:
     if args[1] in self.main.battles[cl.battle].players:
       if args[2].isdigit() and int(args[2]) < 17:
-	tu = self.main.allclients[self.main.clientsusernames[args[1]].sck]
+	tu = self.main.allclients[self.main.clientsusernames[args[1].lower()].sck]
 	tu.battlestatus.teamno = int(args[2])
 	bs = tu.getbattlestatus()
 	self.main.broadcastbattle(cl.battle,"CLIENTBATTLESTATUS %s %s\n" % (tu.username,bs))
